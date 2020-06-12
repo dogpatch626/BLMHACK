0491e630604aa850d4be60fdc3712b14cf7765ea
shemar <dshemar6@gmail.com>
2020-06-12 13:14:59 -0400

    New Front Page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="base">
+        <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="BLM"> BLACK <br/> LIVES <br/> MATTER</h1>
+        <span className="BLM-subtext"> Locate, Review, and Rate your elecetd officals bi-weekly and keep track of their legistrations and bills.</span>
+
+        <div class="col-3-zip">
+            <input class="effect-5" type="text" placeholder="ENTER ZIPCODE"/>
+            <span class="focus-border"></span>
+        </div>
       </header>
+      </div>
+      </div>
     </div>
   );
 }
