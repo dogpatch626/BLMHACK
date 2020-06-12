7281cf28f6c0e82cd2d7cdb3dab3498bd8b46782
shemar <dshemar6@gmail.com>
2020-06-12 18:36:31 -0400

    homepage changes v2

--- src/App.js
+++ src/App.js
@@ -12,30 +12,25 @@ export default class App extends Component {
   state = {
     zipcode: ""
   };
-  
-  zipChangeHandler = e => {
-    
-		this.setState({
-			[e.target.zipcode]: e.target.value
-    });
-    
-    console.log(this.state.zipcode)
 
-  };
+
+  zipChangeHandler(e) {
+    this.setState({zipcode: e.target.value});
+  }
+
   componentDidMount() {
 
   }
   showButton = e =>{
-    console.log("started")
     let b = document.getElementById("sb");
     b.style.display = "inline";
     b.style.animation = "fadeInUp 1s ease-in-out 0s forwards"
-    console.log("done")
-    
   }
 
   changePage = e => {
     e.preventDefault()
+    //Zipcode Here
+    // console.log(this.state.zipcode)
 
     ReactDOM.render(
         <Main />,
@@ -49,13 +44,14 @@ export default class App extends Component {
       <div className="base">
         <div className="container">
       <header className="App-header">
-        <h1 className="BLM"> BLACK <br/> LIVES <br/> MATTER</h1>
+        <h1 className="BLM-header glow">Know Yo Officals</h1>
+        <a href="https://blacklivesmatters.carrd.co/"><h2 className="BLM"> BLACK <br/> LIVES <br/> MATTER</h2></a>
         <span className="BLM-subtext"> Locate, Review, and Rate your elecetd officals bi-weekly and keep track of their legistrations and bills.</span>
 
         <div className="col-3-zip">
             <form class="index-search-form" onSubmit={this.changePage}>
               
-            <input class="effect-5" type="text" onClick={this.showButton} onChange={this.zipChangeHandler} placeholder="ENTER ZIPCODE" type="text" pattern="[0-9]*"/>
+            <input class="effect-5" type="text" onClick={this.showButton} onChange={(e) => this.zipChangeHandler(e)} placeholder="ENTER ZIPCODE" type="text" pattern="[0-9]*"/>
             <span class="focus-border"></span>
             <button id="sb" class="zip-button" name="submit" type="sumbit" ><i class="far fa-arrow-alt-circle-right"></i></button>
             </form>
